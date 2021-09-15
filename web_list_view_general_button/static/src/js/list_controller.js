aba0b3f7b467a5c69e36e7755a40f40a08fc5c85
Ooops CI <noreply@github.com>
2021-09-15 10:22:07 +0000

    [UPD] Update web_list_view_general_button.pot

--- web_list_view_general_button/static/src/js/list_controller.js
+++ web_list_view_general_button/static/src/js/list_controller.js
@@ -1,4 +1,4 @@
-odoo.define("web_list_view_general_buttons.ListController", function(require) {
+odoo.define("web_list_view_general_button.ListController", function(require) {
     "use strict";
 
     var core = require("web.core");
